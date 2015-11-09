a90887c4d27eaa06a118aa7cbdc91009b6164b8f
csthsl <steve@fusecast.com>
2015-11-08 18:56:36 -0700

    Update angular-peity.js
    I added support for donut charts.
    
    Usage: <donut-chart data="vm.DonutChart.data" options="vm.DonutChart.options"></donut-chart>
    
    In the controller code: 
    
    $scope.DonutChart = {
          data: '226/360',
          options: {
            width: 150,
            stroke: "#eee"
          }
        }

--- angular-peity.js
+++ angular-peity.js
@@ -1,58 +1,69 @@
-var angularPeity = angular.module( 'angular-peity', [] );
+var angularPeity = angular.module('angular-peity', []);
 
 
-var buildChartDirective = function ( chartType ) {
-	return {
-		restrict: 'E',
-		scope: {
-			data: "=",
-			options: "="
-		},
-		link: function ( scope, element, attrs ) {
+var buildChartDirective = function (chartType) {
+  return {
+    restrict: 'E',
+    scope: {
+      data: "=",
+      options: "="
+    },
+    link: function (scope, element, attrs) {
 
-            var options = {};
-            if ( scope.options ) {
-				options = scope.options;
-            }
+      var options = {};
+      if (scope.options) {
+        options = scope.options;
+      }
 
-			var span = document.createElement( 'span' );
-			span.textContent = scope.data.join();
+      var span = document.createElement('span');
+      if(chartType != 'donut'){
+        span.textContent = scope.data.join();
+      }else{
+        span.textContent = scope.data;
+      }
 
-            if ( !attrs.class ) {
-                span.className = "";
-            } else {
-                span.className = attrs.class;
-            }
 
-            if (element[0].nodeType === 8) {
-                element.replaceWith( span );
-            } else {
-                element[0].appendChild( span );
-            }
+      if (!attrs.class) {
+        span.className = "";
+      } else {
+        span.className = attrs.class;
+      }
 
-            jQuery( span ).peity( chartType, options );
+      if (element[0].nodeType === 8) {
+        element.replaceWith(span);
+      } else {
+        element[0].appendChild(span);
+      }
 
-		}
-	};
+      jQuery(span).peity(chartType, options);
+
+    }
+  };
 };
 
 
-angularPeity.directive( 'pieChart', function () {
+angularPeity.directive('pieChart', function () {
 
-	return buildChartDirective( "pie" );
+  return buildChartDirective("pie");
 
-} );
-
-
-angularPeity.directive( 'barChart', function () {
-
-	return buildChartDirective( "bar" );
-
-} );
+});
 
 
-angularPeity.directive( 'lineChart', function () {
+angularPeity.directive('barChart', function () {
 
-	return buildChartDirective( "line" );
+  return buildChartDirective("bar");
 
-} );
+});
+
+
+angularPeity.directive('lineChart', function () {
+
+  return buildChartDirective("line");
+
+});
+
+angularPeity.directive('donutChart', function () {
+
+  return buildChartDirective("donut");
+
+});
